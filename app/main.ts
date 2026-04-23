b2ea9ab63c57c81e4010dd6ab32830c2456f08c9
Adam <adammegarules1@proton.me>
2026-04-23 20:27:49 +0200

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -31,8 +31,18 @@ async function main() {
 
   console.error("Logs from your program will appear here!");
 
-  console.log(response.choices[0].message.content);
-  console.log(response);
+  const message = response.choices[0].message;
+  if (message.tool_calls && message.tool_calls.length > 0) {
+    const toolCall = message.tool_calls[0];
+    const functionName = toolCall.funcion.name;
+    const args = JSON.parse(toolCall.function.arguments);
+    if (functionName === "Read") {
+      const content = fs.readFileSync(args.file_path, "utf-8");
+      console.log(content);
+    }
+  } else {
+    console.log(message.content);
+  }
 }
 
 main();
