62b7677a53b280dabd9c88a9739e9026b1d5ddb6
Adam <adammegarules1@proton.me>
2026-04-23 20:50:18 +0200

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,9 @@
 import OpenAI from "openai";
 import read from "./tools/read.json";
-import fs from "fs"
+import fs from "fs";
+
+let tools = [read];
+
 async function main() {
   const [, , flag, prompt] = process.argv;
   const apiKey = process.env.OPENROUTER_API_KEY;
@@ -18,30 +21,47 @@ async function main() {
     apiKey: apiKey,
     baseURL: baseURL,
   });
-
-  const response = await client.chat.completions.create({
-    model: "anthropic/claude-haiku-4.5",
-    messages: [{ role: "user", content: prompt }],
-    tools: [read],
-  });
-
-  if (!response.choices || response.choices.length === 0) {
-    throw new Error("no choices in response");
-  }
+  let message = [{ role: "user", content: prompt }];
 
   console.error("Logs from your program will appear here!");
+  while (true) {
+    const response = await client.chat.completions.create({
+      model: "anthropic/claude-haiku-4.5",
+      messages: message,
+      tools: tools,
+    });
 
-  const message = response.choices[0].message;
-  if (message.tool_calls && message.tool_calls.length > 0) {
-    const toolCall = message.tool_calls[0];
-    const functionName = toolCall.function.name;
-    const args = JSON.parse(toolCall.function.arguments);
-    if (functionName === "Read") {
-      const content = fs.readFileSync(args.file_path, "utf-8");
-      console.log(content);
+    if (!response.choices || response.choices.length === 0) {
+      throw new Error("no choices in response");
     }
-  } else {
-    console.log(message.content);
+
+    const responseMessage = response.choices[0].message;
+
+    //append response message to messages
+    messages.push({
+      role: "assistant",
+      content: responseMessage ?? null,
+      ...(responseMessage.tool_calls
+        ? { tool_calls: responseMessage.tool_calls }
+        : {}),
+    });
+
+    if (responseMessage.tool_calls && responseMessage.tool_calls.length > 0) {
+      const toolCall = responseMessage.tool_calls[0];
+      const functionName = toolCall.function.name;
+      const args = JSON.parse(toolCall.function.arguments);
+      if (functionName === "Read") {
+        const content = fs.readFileSync(args.file_path, "utf-8");
+        message.push({
+          role: "tool",
+          tool_call_id: toolCall.id,
+          content: content,
+        });
+      } // use for diffrent tool_calls
+    } else {
+      console.log(responseMessage.content);
+      break;
+    }
   }
 }
 
