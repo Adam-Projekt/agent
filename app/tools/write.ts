700feddda87c0c59661747d3aed816e981b933cc
Adam <adammegarules1@proton.me>
2026-04-24 07:35:38 +0200

    codecrafters submit [skip ci]

--- app/tools/write.ts
+++ app/tools/write.ts
@@ -1,6 +1,6 @@
 import fs from "fs";
 
-export function write(args) {
+export function write(args: any) {
   const path = args.file_path;
   fs.writeFileSync(path, args.content);
   return "Successfully wrote file";
